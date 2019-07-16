9babb9d48a37dcd906fda73a5bab7d6471a39720
kiat-mao <kiat.mao@gmail.com>
2019-07-16 11:11:20 +0800

    add generate_no and validates of uniq name in same supplier

--- app/models/commodity.rb
+++ app/models/commodity.rb
@@ -2,14 +2,17 @@ class Commodity < ActiveRecord::Base
 	belongs_to :supplier
 
 	# has_and_belongs_to_many :Order
-    mount_uploader :cover, CoverUploader
+  mount_uploader :cover, CoverUploader
 	# validates_presence_of :cno, :dms_no, :name, :supplier_id, :cost_price, :sell_price, :message => '不能为空'
 	# validates_uniqueness_of :cno, :message => '该商品编码已存在'
 
 	has_many :order_details
 
-	validates :cno, :dms_no, :name, :supplier_id, :cost_price, :sell_price, presence: {:message => "不能为空"}
-	validates :cno, uniqueness: {:message => "该商品编码已存在"}
+	before_create :generate_no
+
+	validates :dms_no, :name, :supplier_id, :cost_price, :sell_price, presence: {:message => "不能为空"}
+	validates :cno, :dms_no, uniqueness: {:message => "该商品编码已存在"}
+	validates :name, uniqueness: {scope: :supplier,:message => "该供应商下已存在同名商品"}
 	validates :cost_price, :sell_price, numericality: {:greater_than => 0 }
 
 
@@ -30,4 +33,10 @@ class Commodity < ActiveRecord::Base
 			return false
 		end
 	end
+
+	private
+	def generate_no
+    count = (Commodity.where(supplier: self.supplier).count + 1).to_s.rjust(5, '0')
+    self.cno = "#{self.supplier.sno}-#{count}"
+  end
 end
